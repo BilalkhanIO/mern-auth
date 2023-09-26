88dd9ad7c20d2603cd6a81cb5f5d56e5b00f48c9
bilalkhanio <bilalkhan301dir@gmail.com>
2023-09-26 17:20:47 +0500

    Create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,13 @@ import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUP from "./pages/SignUP";
 import Profile from "./pages/Profile";
+import Header from "./components/header/Header";
 const App = () => {
   return (
+    <>
+    <header>
+      <Header/>
+    </header>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
@@ -14,6 +19,7 @@ const App = () => {
       <Route path="/sign-up" element={<SignUP />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
+    </>
   );
 };
 
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { Popover } from "@headlessui/react";
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+} from "@heroicons/react/24/outline";
+import {
+  ChevronDownIcon,
+  PhoneIcon,
+  PlayCircleIcon,
+} from "@heroicons/react/20/solid";
+import MobileNavBar from "./MobileNavBar";
+import { Link } from "react-router-dom";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="bg-white">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <Link to="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <Popover.Group className="hidden lg:flex lg:gap-x-12">
+          <Link
+            to="/"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Home
+          </Link>
+          <Link
+            to="/about"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            About
+          </Link>
+          <Link
+            to="/profile"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Profile
+          </Link>
+        </Popover.Group>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <Link
+            to="/sign-in"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Log in <span aria-hidden="true">&rarr;</span>
+          </Link>
+        </div>
+      </nav>
+      <MobileNavBar
+        mobileMenuOpen={mobileMenuOpen}
+        setMobileMenuOpen={setMobileMenuOpen}
+      />
+    </header>
+  );
+}
