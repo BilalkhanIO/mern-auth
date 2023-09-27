b8b880c0b686601bc92e016d9e92eb63051ca204
bilalkhanio <bilalkhan301dir@gmail.com>
2023-09-27 10:29:05 +0500

    create user modal

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error } from "console";
 
+dotenv.config();
+
+//express server
 const app = express();
 
+//mongoose connection
+mongoose
+  .connect(process.env.MONGOO)
+  .then(() => {
+    console.log("server is connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+//
 app.listen(3000, () => {
   console.log("server is runing...");
 });
