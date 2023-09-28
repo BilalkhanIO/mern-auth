14261834b8720efecc149a2b6bb61518ff1201f2
bilalkhanio <bilalkhan301dir@gmail.com>
2023-09-28 17:16:12 +0500

    add  redux persist to store cerdentials locally

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import { persistor, store } from "./store/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>
 );
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,28 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/userSlice";
+
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistedReducer = persistReducer( persistConfig, rootReducer );
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export let persistor = persistStore(store);
