a3fbf224807389040947b2e6379e32f75f3d7e07
bilalkhanio <bilalkhan301dir@gmail.com>
2023-09-28 12:41:01 +0500

    create sign in route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import Jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
@@ -12,5 +13,25 @@ export const signup = async (req, res, next) => {
     res.status(201).json({ message: "user create successfully" });
   } catch (error) {
     next(errorHandler(300, "Something went worring"));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not fund"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "wrong credentials"));
+    }
+    const token = Jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashpassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 10800000);
+    res.cookie("access_toke", token, { httpOnly: true , expires: expiryDate}).status(200).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
