b58b791676c3de45d2b183f24c6c63740d4ece50
bilalkhanio <bilalkhan301dir@gmail.com>
2023-10-05 10:43:18 +0500

    cretae and update user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,37 @@
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
 export const test = (req, res) => {
-    res.json({
-      message: "api is working",
-    });
+  res.json({
+    message: "api is working",
+  });
+};
+
+export const updatedUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "you can update only your account"));
   }
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          userName: req.body.userName,
+          email: req.body.email,
+          password: req.body.password,
+          profilepicture: req.body.profilepicture,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+
+  } catch (error) {
+    next(error)
+  }
+};
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 //mongoose connection
 mongoose
